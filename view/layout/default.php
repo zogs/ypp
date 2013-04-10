57b6030b99f9546407c7cdb0c186f92de49e3aec
Mr.zogs <guichardsim@gmail@gmail.com>
2013-04-10 12:03:15 +0200

    footercordeon

--- view/layout/default.php
+++ view/layout/default.php
@@ -88,6 +88,25 @@
 		<?php echo $content_for_layout;?>
 	</div>
 
+	<footer id="footer">
+		<div id="foo-one"></div>
+		<div id="foo-sign"><img src="<?php echo Router::webroot('img/sign.png');?>" alt=""></div>
+		<div id="foo-two">
+			<div class="container foo-content">				
+				<div class="fright">YouProtest</div>
+			</div>
+		</div>
+	</footer>
+
+	<style>
+		#footer { position:fixed; bottom:-120px; height:150px; z-index:20;width:100%; background-color: rgba(0,0,0,0.1);}
+		#foo-one { position:absolute; top:30px; height:120px;  z-index:-20; width:100%; background-color: rgba(0,0,0,0.5);}
+		#foo-two { position:absolute; top:30px; z-index:-10; width:100%; background-color: rgba(0,0,0,1);}
+		#foo-sign {position:absolute; top:30px; height: 250px; left:30%; z-index:-15;}
+		#foo-sign img {height:200px;}
+
+		.foo-content{ padding:10px 0;}
+	</style>
 
 </body>
 
@@ -95,6 +114,37 @@
 
  <script type="text/javascript">
 
+
+ 	$(document).ready(function(){
+
+
+ 		var timeout;
+ 		var opened = false;
+ 		$("#footer").hover(
+			function () {
+
+				timeout = setTimeout(function(){ 
+
+					if(opened==false){
+						$('#foo-two').animate({top:'-='+$('#foo-two').height()}, 150, 'linear');
+						$('#foo-one').delay(50).animate({top:'-='+$('#foo-one').height()}, 150, 'linear');
+						$('#foo-sign').delay(50).animate({top:'-='+$('#foo-sign').height()}, 250, 'linear',function(){ opened = true; });
+					}
+				},300);
+			},
+			function () {
+
+				clearTimeout(timeout);
+				if(opened==true){
+					
+					$('#foo-two').animate({top:'+='+$('#foo-two').height()}, 100);
+					$('#foo-one').animate({top:'+='+$('#foo-one').height()}, 100);
+					$('#foo-sign').animate({top:'+='+$('#foo-sign').height()}, 150, 'linear',function(){ opened=false });
+				}
+			}
+			);
+
+ 	});
  	/*===========================================================
  		Set security token
  	============================================================*/
@@ -110,20 +160,20 @@
  	/*===========================================================
  		GOOGLE FONTS
  	============================================================*/
-      WebFontConfig = {
-        google: { families: [ 'Bangers','Squada One','Oswald:300,400,700' ] },      
-        fontinactive: function(fontFamily, fontDescription) { /*alert('Font '+fontFamily+' is currently not available'); */}
-      };
+      // WebFontConfig = {
+      //   google: { families: [ 'Bangers','Squada One','Oswald:300,400,700' ] },      
+      //   fontinactive: function(fontFamily, fontDescription) { /*alert('Font '+fontFamily+' is currently not available'); */}
+      // };
 
-      (function() {
-        var wf = document.createElement('script');
-        wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
-            '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
-        wf.type = 'text/javascript';
-        wf.async = 'true';
-        var s = document.getElementsByTagName('script')[0];
-        s.parentNode.insertBefore(wf, s);
-      })();
+      // (function() {
+      //   var wf = document.createElement('script');
+      //   wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+      //       '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+      //   wf.type = 'text/javascript';
+      //   wf.async = 'true';
+      //   var s = document.getElementsByTagName('script')[0];
+      //   s.parentNode.insertBefore(wf, s);
+      // })();
 </script>
 
 
